3b756d60bedd1cf430128d2c8c7ef3e9fa8c1504
TAKINUR <takinurm@gmail.com>
2022-08-02 17:05:34 +0600

    Fixing vite config for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/vanity-rekt/",
   plugins: [react()],
   esbuild: {
     logOverride: { 'this-is-undefined-in-esm': 'silent' }
